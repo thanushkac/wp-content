6a12d0c787e2b0c93399b8a7728c3ffdf2e41d88
Thanushka <thanushka@gmail.com>
2015-04-05 19:27:29 -0400

    buildingspage

--- themes/Olympia2015/page-buildings.php
+++ themes/Olympia2015/page-buildings.php
@@ -70,82 +70,106 @@ yoast_breadcrumb('<p id="breadcrumbs">','</p>');
 
                 </div><!--end row-->
                 
-                
+                <hr/>
                 
                 
                 	<div class="row row-top-padding"><!--start row-->
                 
-                    <div class="col-md-12 image-gallery"> <!--start column porlet-->
+                    <div class="col-md-6 image-gallery"> <!--start column porlet-->
                     <p class="h3">About this Building </p>
-                    <p class="two-column">Olympia Steel Buildings offers garages, garage kits and storage sheds that provide a home for your valuable investments keeping them safe and secure in a convenient, easily accessible space of their own. A steel building can be constructed in less time than it takes to erect a conventional building at a much lower cost. Warranties on the building's roof, paint and fasteners will increase your peace of mind and virtually no maintenance will increase the time you have to spend with the items housed in your new Olympia building.</p>
+                    <p>Olympia Steel Buildings offers garages, garage kits and storage sheds that provide a home for your valuable investments keeping them safe and secure in a convenient, easily accessible space of their own. A steel building can be constructed in less time than it takes to erect a conventional building at a much lower cost. Warranties on the building's roof, paint and fasteners will increase your peace of mind and virtually no maintenance will increase the time you have to spend with the items housed in your new Olympia building.</p>
                     </div> <!--end column porlet-->
+                    
+                    <div class="col-md-6 image-gallery"> <!--start column porlet-->
+                    <p class="h3">Let's Get Started!</p>
+                    <p>To inquire about a new garage and save up to 50% over conventional construction, get a free online quote on the building of your choice or call toll-free at 1-888-449-7756 for information and pricing.</p>
+                    <p class="text-center"><a href="#" class="btn btn-info btn-lg img-responsive full-width-btn" role="button">Get a Price Estimate</a></p>
+                    </div> <!--end column porlet-->
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                     
                     <!-- badges start-->
                     <div class="row top-macro-spacing"></div>
-<div class="row dark-bg-pattern">
+                    <div class="row top-macro-spacing"></div>
+                    
+<div class="container dark-bg-pattern">
 <div>
 <p class="h2 text-center white-text">All Olympia steel buildings comes with</p>
 
 </div>
 <div class="row">
-<div class="col-md-2 badges text-center">
+<div class="col-xs-4 badges text-center">
 
 <img class="img-responsive aligncenter" src="http://localhost:8888/wordpress/wp-content/uploads/2015/03/enginered.png" alt="enginered" />
 <p class="h3">Engineered</p>
 <p class="h6">to last a lifetime</p>
 
 </div>
-<div class="col-md-2 badges text-center">
+<div class="col-xs-4 badges text-center">
 
 <img class="img-responsive aligncenter" src="http://localhost:8888/wordpress/wp-content/uploads/2015/03/35.png" alt="35" />
 <p class="h3">35 Year</p>
-<p class="h6">Rust-through perforation warranty on AZ55 Galvalume® roof panels</p>
+<p class="h6">Rust through perferation warrenty</p>
 
 </div>
-<div class="col-md-2 badges text-center">
+<div class="col-xs-4 badges text-center">
 
 <img class="img-responsive aligncenter" src="http://localhost:8888/wordpress/wp-content/uploads/2015/03/40.png" alt="40" />
 <p class="h3">40 Year</p>
 <p class="h6">Warrenty on paint</p>
 
 </div>
-
-
-
-<div class="col-md-2 badges text-center">
-
-<img class="img-responsive aligncenter" src="http://localhost:8888/wordpress/wp-content/uploads/2015/03/40.png" alt="40" />
-<p class="h3">40 Year</p>
-<p class="h6">Warrenty on paint</p>
-
-</div>
-
-
-
-<div class="col-md-2 badges text-center">
-
-<img class="img-responsive aligncenter" src="http://localhost:8888/wordpress/wp-content/uploads/2015/03/40.png" alt="40" />
-<p class="h3">40 Year</p>
-<p class="h6">Warrenty on paint</p>
-
-</div>
-
-
-<div class="col-md-2 badges text-center">
-
-<img class="img-responsive aligncenter" src="http://localhost:8888/wordpress/wp-content/uploads/2015/03/40.png" alt="40" />
-<p class="h3">40 Year</p>
-<p class="h6">Warrenty on paint</p>
-
-</div>
-
-
 </div>
 </div>
 <!-- badges end-->
                     
 
                 </div><!--end row-->
+                
+                
+                
+                <div class="row top-macro-spacing"></div>
+                    <div class="row top-macro-spacing"></div>
+                    
+                    
+                    
+                    
+                    
+            <div class="container"> <!--CONTAINER START-->
+            
+            
+            <div class="row">
+            <div class="col-md-12 text-center">
+            <p class="h2">Why People Choose Olympia</p>
+            </div>
+            </div>
+            
+              <div class="row">
+              <div class="col-sm-6">
+              <p class="h5">Years of metal storage building design experience</p>
+              <p>
+              providing quality products at savings of up to 50% over conventional construction.
+              </p>
+              </div>
+              <div class="col-sm-6">
+              </div>
+              
+              </div> 
+            </div>    <!--CONTAINER END--> 
+                    
+                
+                
+                
                 
                 
                 
